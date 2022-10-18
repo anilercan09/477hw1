e4e7d384d0cf2d9cf99ec56854271845dc6894fd
anilercan09 <anil.ercan09@gmail.com>
2022-10-18 20:12:23 +0300

    first commit

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+struct Vec3f
+    {
+        float x, y, z;
+    };
+
+int main()
+{
+    int x=0;
+    
+    return 0;
+
+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
+
+}
